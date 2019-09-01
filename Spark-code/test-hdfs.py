88c4a2c43b76d77121d91d7800a986bbd2e5d40c
magic_developer@hotmail.com <Mohamed.Abdalglil@kagsi.com>
2019-09-01 12:41:14 +0300

    ok

---
+++ Spark-code/test-hdfs.py
@@ -0,0 +1,14 @@
+﻿%spark.pyspark
+
+from pyspark import SparkConf, SparkContext
+
+sc.stop()
+
+conf = SparkConf().setAppName('testappx23').setMaster('spark://e280bfb76255:7077')
+
+sc = SparkContext(conf=conf).getOrCreate()
+ 
+
+content = sc.textFile("hdfs://namenode:9000/hdfs_test")
+
+content.collect()
